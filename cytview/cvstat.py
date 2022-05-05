b195449b78681038a9106caf7269259a36f5c025
Charlie Bidgood <charlesliambidgood@gmail.com>
2022-05-05 14:19:43 +1000

    Initial Commit

---
+++ cytview/cvstat.py
@@ -0,0 +1,76 @@
+import matplotlib.pylab as plt
+import numpy as np
+import scipy.stats as scipy
+
+def significance(p):
+    
+   if (p < 0.0001): return("****")
+   elif(p < 0.001): return("***")
+   elif (p < 0.01): return("**")
+   elif (p < 0.05): return("*")
+   elif (p > 0.05): return("ns")
+   else: return("")
+
+
+def draw_lines(p_value, compare, groupings, box_data):
+    # padding counter to prevent overlap of connecting lines
+    pad_counter = np.full(shape=len(groupings), fill_value =0)
+    
+    n = 0
+    while n < len(compare):
+        
+        # pull boxplot data and esitamate the whisker length for both boxes
+        b1_iqr = box_data.iloc[:,compare[n][0]]["75%"]
+        b1_mean = box_data.iloc[:,compare[n][0]]["mean"]
+        b1_whis = b1_iqr - b1_mean
+
+        b2_iqr = box_data.iloc[:,compare[n][1]]["75%"]
+        b2_mean = box_data.iloc[:,compare[n][1]]["mean"]
+        b2_whis = b2_iqr - b2_mean
+
+        # calculate y distances respective to the padding counter
+        height_adjust = max(pad_counter[compare[n][0]:compare[n][1]+1])
+        b1_ydist = b1_iqr + (b1_whis * (height_adjust+1)*2)
+        b2_ydist = b2_iqr + (b2_whis * (height_adjust+1)*2)
+
+        # draw connecting line
+        linemax = max(b1_ydist, b2_ydist)
+
+        plt.plot([compare[n][0],compare[n][1]], [linemax, linemax], color ="black")
+
+        # calculate down line distances and shorten tem respective to padding counter
+        b1_dist = ((linemax - (b1_iqr + 0.75*b1_whis))) / (pad_counter[compare[n][0]] + 1)
+        b2_dist = ((linemax - (b2_iqr + 0.75*b2_whis))) / (pad_counter[compare[n][1]] + 1) 
+
+        # draw down lines
+        plt.plot([compare[n][0],compare[n][0]], [linemax, linemax - b1_dist], color ="black")
+        plt.plot([compare[n][1],compare[n][1]], [linemax, linemax - b2_dist], color ="black")
+
+        # show significance symbols
+        text_coord = (compare[n][1] + compare[n][0]) / 2
+        plt.text(text_coord, linemax, significance(p_value), ha="center", fontsize=20)
+
+        # add to padding counters
+        pad_counter[compare[n][0]] = pad_counter[compare[n][0]] + 1
+        pad_counter[compare[n][1]] = pad_counter[compare[n][1]] + 1
+
+        n = n + 1
+
+
+def multi_comparison(dataframe, compare, groupings, labels, box_data, draw):
+    
+    print(box_data)
+      
+    n = 0
+    while n < len(compare):
+
+        control_means =  np.mean(dataframe.loc[:,groupings[compare[n][0]]])
+        test_means = np.mean(dataframe.loc[:,groupings[compare[n][1]]])
+        
+        p_value = scipy.f_oneway(control_means, test_means)[1]
+        print(labels[compare[n][0]], "vs", labels[compare[n][1]], ": p value: ", str(round(p_value,6)))
+               
+        n = n + 1
+        
+    if draw:
+        draw_lines(p_value, compare, groupings, box_data)
---
+++ cytview/cytview.py
@@ -0,0 +1,62 @@
+import seaborn as sns
+import matplotlib.pylab as plt
+import pandas as pd
+import numpy as np
+import warnings
+
+import cvstat
+
+def point_plot(dataframe, measurment, identifier, groupings, labels, y_label=None, color='salmon', compare=None, figsize=None, draw=False):
+
+    # generate a list of all values for each unique identifier
+    combined_object = dataframe.groupby(identifier)[measurment].apply(list)
+    
+    # convert the set of lists to a dictionary and allow for unequal array lengths
+    combined_object = pd.DataFrame({k: pd.Series(l) for k, l in combined_object.items()}).apply(pd.to_numeric)
+    
+    # cut off dataframe to 1000 sample observations to prevent computational strain
+    combined_object = combined_object[0:1000] 
+ 
+    # generate a dataframe containing the means of replicate samples
+    grouped_df = pd.DataFrame([]) 
+    means = pd.DataFrame([]) 
+
+    for x in groupings:
+        values = combined_object[x]
+        means['mean'] = values.mean(axis=1)
+        grouped_df = pd.concat([grouped_df, means['mean']], axis=1)
+        
+    for column in range(grouped_df.shape[1]):
+        grouped_df.columns.values[column] = labels[column]
+    
+    
+    # plotting functions
+    figure = plt.figure(figsize=figsize)
+    plt.ylabel(y_label, fontsize = 15)
+
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        warnings.warn("UserWarning arose", UserWarning)
+        sns.swarmplot(data=grouped_df, size=3, zorder=0.5, color=color)
+    
+    sns.boxplot(data=grouped_df, boxprops=dict(alpha=.5), whis=0.3,
+                color="black", sym='')
+
+    # collect boxplot details for statistical comparison and visualisation
+    box_data = grouped_df.describe()
+    
+    if(compare==None):
+        pass
+    else:
+        cvstat.multi_comparison(dataframe = combined_object, compare=compare, 
+                                 groupings=groupings, labels=labels, box_data=box_data, draw=draw)
+    
+    control_line = np.median(grouped_df.iloc[:,0])
+    plt.axhline(y=control_line, color='black', linestyle='--')
+    plt.tight_layout()
+    plt.show()
+
+
+
+
+
