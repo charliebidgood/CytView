8e06c0fb253df437eb34fb8752334575e8989ba1
Charlie Bidgood <charlesliambidgood@gmail.com>
2022-07-01 09:11:47 +1000

    shared dataset extraction function, updated example notebook

--- cytview/cytview.py
+++ cytview/cytview.py
@@ -6,39 +6,47 @@ import warnings
 
 from cytview import cvstat
 
-def cell_plot(dataframe, measurment, identifier, obs_max = 1000, color="Accent"):
+def extract_values(dataframe, measurment, identifier, obs_max=500):
     
-    dataframe = dataframe[0:obs_max]
+    #generate a list of all values for each unique identifier
+    extracted_df = dataframe.groupby(identifier)[measurment].apply(list)
+    # convert the set of lists to a dictionary and allow for unequal array lengths
+    extracted_df = pd.DataFrame({k: pd.Series(l) for k, l in extracted_df.items()}).apply(pd.to_numeric).dropna()
+  
+    extracted_df = extracted_df.sample(n=obs_max)
+    return(extracted_df)
 
+
+def cell_plot(dataframe, measurment, identifier, obs_max = 500, color="Accent"):
+       
+    extracted_df = extract_values(dataframe, measurment, identifier, obs_max)
+      
     with warnings.catch_warnings():
         
         warnings.simplefilter("ignore")
         warnings.warn("UserWarning arose", UserWarning)
         
         pal = sns.set_palette(sns.color_palette(color))
-        sns.swarmplot(data=dataframe, size=3, zorder=0.5,  palette=pal)
+        sns.swarmplot(data=extracted_df, size=3, zorder=0.5,  palette=pal)
 
-    sns.boxplot(data=dataframe, boxprops=dict(alpha=.5), whis=0.3,
+    sns.boxplot(data=extracted_df, boxprops=dict(alpha=.5), whis=0.3,
                 color="black", sym='')
 
+    results = {"dataframe": extracted_df, "summary": extracted_df.describe()}
+    return(results)
 
-def group_plot(dataframe, measurment, identifier, groupings, labels, obs_max = 1000, color="Accent", compare=None, figsize=None, draw=False):
 
-    # generate a list of all values for each unique identifier
-    combined_object = dataframe.groupby(identifier)[measurment].apply(list)
-    
-    # convert the set of lists to a dictionary and allow for unequal array lengths
-    combined_object = pd.DataFrame({k: pd.Series(l) for k, l in combined_object.items()}).apply(pd.to_numeric)
-    
-    # cut off dataframe to 1000 sample observations to prevent computational strain
-    combined_object = combined_object[0:obs_max] 
+
+def group_plot(dataframe, measurment, identifier, groupings, labels, obs_max = 1000, color="Accent", compare=None, draw=False):
+
+    extracted_df = extract_values(dataframe, measurment, identifier, obs_max)
     
     # generate a dataframe containing the means of replicate samples
     grouped_df = pd.DataFrame([]) 
     means = pd.DataFrame([]) 
 
     for x in groupings:
-        values = combined_object[x]
+        values = extracted_df[x]
         means['mean'] = values.mean(axis=1)
         grouped_df = pd.concat([grouped_df, means['mean']], axis=1)
         
@@ -58,15 +66,14 @@ def group_plot(dataframe, measurment, identifier, groupings, labels, obs_max = 1
     
     sns.boxplot(data=grouped_df, boxprops=dict(alpha=.5), whis=0.3,
                 color="black", sym='')
-
-    # collect boxplot details for statistical comparison and visualisation
-    summary = grouped_df.describe()
-    
+  
     if(compare!=None):
-        cvstat.multi_comparison(dataframe = combined_object, compare=compare, 
-                                 groupings=groupings, labels=labels, summary=summary, draw=draw)
+        cvstat.multi_comparison(dataframe = extracted_df, compare=compare, groupings=groupings,
+                                labels=labels, summary=grouped_df.describe(), draw=draw)
            
-    return(summary)
+    results = { "dataframe": grouped_df, "summary": grouped_df.describe()}
+
+    return(results)
 
 
 
