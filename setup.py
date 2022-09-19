2fe03203b514abc5da5f906b1da126cf68379545
Charlie Bidgood <charlesliambidgood@gmail.com>
2022-09-19 13:24:21 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     install_requires=['matplotlib==3.3.4', 
                       'seaborn==0.11.1',
                       'numpy==1.21.6', 
-                      'pandas==1.1.5',
+                      'pandas==1.3.0',
                       'scipy==1.7.3'],
     version='1.0.0',
     license='MIT',
