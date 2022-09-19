b35e77267a4a5947cfedbbbe155acdaadbd0e94f
Charlie Bidgood <charlesliambidgood@gmail.com>
2022-09-19 11:43:11 +1000

    Updated Numpy Dependency

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=['cytview'],
     install_requires=['matplotlib==3.3.4', 
                       'seaborn==0.11.1',
-                      'numpy==1.20.1', 
+                      'numpy==1.23.3', 
                       'pandas==1.1.5',
                       'scipy==1.7.3'],
     version='1.0.0',
